747c7c7d55e7313411f8c1c0c6d2442b4196bad3
Africa-t0t0 <geoorgex@gmail.com>
2021-12-06 17:18:26 -0300

    Final

---
+++ tarea2arqui/handler.py
@@ -0,0 +1,19 @@
+import requests, json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def handle(request):
+    urldolar = "https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar?apikey=4422286a2b6538121f331725568810b163fc4238&formato=json"
+    resdolar = requests.get(urldolar, verify=False)
+    urltemp = "https://api.openweathermap.org/data/2.5/weather?q=Santiago&units=metric&lang=es&appid=32a86e5ed5641b5c40972f4af8e0cc07"
+    restemp = requests.get(urltemp)
+    urluf = "https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=4422286a2b6538121f331725568810b163fc4238&formato=json"
+    resuf = requests.get(urluf,verify=False)
+    urlcru = "https://www.crucigrama.org/index.php?groupid=Nivel+2+medio%3A+Definiciones&Submit=Generar+nuevo&max_words=10&ActivarSopa=0&cols=15&rows=15"
+    temp = (restemp.json())['main']['temp']
+    dolar = (resdolar.json())['Dolares']
+    uf = resuf.json()['UFs']
+    cru = urlcru
+    resp = ("Temperatura: ", temp, "C", "Dolar: ", dolar, "UF: ", uf, "Crucigrama: ", cru)
+    return resp
+
+
